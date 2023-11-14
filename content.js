d40a238ee62f7d21170efc7abdb76bc646c3702d
Aubrian <aubrian.duhayon@mail-ecv.fr>
2023-11-14 16:01:05 +0100

    :wrench: Manifest v3

--- content.js
+++
@@ -1,7 +0,0 @@
-const keywords = ['durabilité', 'vert', 'écologique', 'bio', 'images']; // Ajoutez plus de mots-clés ici
-const regex = new RegExp('(' + keywords.join('|') + ')', 'gi');
-
-document.body.innerHTML = document.body.innerHTML.replace(regex, match => {
-    console.log("J'AI TROUVE", match, regex);
-  return `<span style="background-color: yellow;">${match}</span>`;
-});
---
+++ detector.js
@@ -0,0 +1,28 @@
+class Detector {
+    words = [];
+    wordsDetected = {};
+
+    constructor(words) {
+        this.words = words;
+    }
+
+    detect() {
+        // Search in the DOM for the words and count them
+        document.body.innerText.split(' ').forEach(word => {
+            if (this.wordsDetected[word]) {
+                this.wordsDetected[word]++;
+            } else {
+                this.wordsDetected[word] = 1;
+            }
+        });
+
+        return this.wordsDetected;
+    }
+
+    static create() {
+        return new Detector(['durabilité', 'vert', 'écologique', 'bio']);
+    }
+}
+
+const detector = Detector.create();
+console.log(detector.detect());
