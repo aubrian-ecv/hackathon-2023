f83f348718782c874f6cd3e0f15e704488ad223e
Aubrian <aubrian.duhayon@mail-ecv.fr>
2023-11-15 12:07:31 +0100

    :sparkles: Ajout de mots à détecter

--- src/popup.js
+++ src/popup.js
@@ -3,18 +3,113 @@ const button = document.getElementById("button");
 
 // FUNCTIONS
 function getWords() {
-    const words = ['durabilité', 'vert', 'écologique', 'bio', 'Summary']
+    const words = [
+        'Durabilité',
+        'Écologie',
+        'Environnement',
+        'Biologique',
+        'Recyclage',
+        'Compostage',
+        'Biodiversité',
+        'Écosystème',
+        'Renouvelables',
+        'Conservation',
+        'Carbone',
+        'Bioénergie',
+        'Éolienne',
+        'Solaire',
+        'Hydroélectricité',
+        'Biomasse',
+        'Géothermie',
+        'Ressources',
+        'Habitat',
+        'Faune',
+        'Flore',
+        'Protection',
+        'Climatique',
+        'Serre',
+        'Neutralité',
+        'ZéroDéchet',
+        'Responsable',
+        'Éthique',
+        'Mobilité',
+        'Permaculture',
+        'Aquaculture',
+        'Foresterie',
+        'Pêche',
+        'Écoconception',
+        'Écolabel',
+        'Pollution',
+        'Déforestation',
+        'Reforestation',
+        'Réutilisation',
+        'Valorisation',
+        'Épuration',
+        'Gestion',
+        'Économie',
+        'Écotourisme',
+        'Biologique',
+        'Équitables',
+        'Équitable',
+        'Environnementale',
+        'Éducation',
+        'Activisme',
+        'Technologie',
+        'Électrique',
+        'Thermique',
+        'Énergétique',
+        'Hydrique',
+        'Risques',
+        'Sociétale',
+        'Affaires',
+        'Innovation',
+        'Urbanisme',
+        'Bâtiment',
+        'Architecture',
+        'Ville',
+        'Écosocialisme',
+        'Transition',
+        'Réseaux',
+        'Capteurs',
+        'Indicateur',
+        'CycleVie',
+        'ÉconomieBleue',
+        'ÉconomieVerte',
+        'Biomimétisme',
+        'Corridor',
+        'Propre',
+        'Eaux',
+        'QualitéAir',
+        'Conservation',
+        'Refuge',
+        'Régénérative',
+        'Végétalisé',
+        'Jardinage',
+        'Transport',
+        'Covoiturage',
+        'MicroMobilité',
+        'MarchéCarbone',
+        'CréditCarbone',
+        'Compensation',
+        'Investissement',
+        'FondsVert'
+    ];
     const wordsDetected = {};
-    document.body.innerText.split(' ').forEach(word => {
-        if (words.indexOf(word) !== -1) {
-            if (wordsDetected[word]) {
-                wordsDetected[word]++;
+    const allPageWords = document.body.innerText.split(/\s+/);
+    allPageWords.forEach(word => {
+        if (words.map(w => w.toLowerCase()).indexOf(word.toLowerCase()) !== -1) {
+
+            const matchedWord = word.toLowerCase();
+
+            if (wordsDetected[matchedWord]) {
+                wordsDetected[matchedWord]++;
             } else {
-                wordsDetected[word] = 1;
+                wordsDetected[matchedWord] = 1;
             }
         }
     });
-    return wordsDetected;
+
+    return { allPageWords, wordsDetected };
 }
 
 async function getCarbonData(url) {
@@ -41,7 +136,7 @@ async function getTabUrl() {
     return tabs[0].url;
 }
 
-button && button.addEventListener("click", async () => {
+(async () => {
     const queryOptions = { active: true, currentWindow: true };
     const tabs = await chrome.tabs.query(queryOptions);
 
@@ -63,14 +158,19 @@ button && button.addEventListener("click", async () => {
             for (const { frameId, result } of injectionResults) {
                 console.log(`Frame ${frameId} result:`, result);
                 const wordsList = document.getElementById('words-list');
+                const carbonSpan = document.getElementById('carbon');
                 if (wordsList) {
                     wordsList.innerHTML = '';
-                    for (const word in result) {
+                    for (const word in result.wordsDetected) {
                         const li = document.createElement('li');
-                        li.innerText = `${word} : ${result[word]}`;
+                        li.innerText = `${word} : ${result.wordsDetected[word]}`;
                         wordsList.appendChild(li);
                     }
                 }
+
+                if (carbonSpan) {
+                    carbonSpan.innerText = result.allPageWords.length;
+                }
             }
         });
-});
+})();
